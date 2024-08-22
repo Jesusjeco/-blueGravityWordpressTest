05c721cce37ea22c321c6b2d8c5f0606234e1ba3
Jesus Carrero <jesusenrique.carrero@gmail.com>
2024-08-21 22:26:46 -0400

    Adding page template

--- wp-content/themes/jecoWordpressShell/page.php
+++ wp-content/themes/jecoWordpressShell/page.php
@@ -1,4 +1,10 @@
 <?php
-get_header();
-the_content();
+get_header(); ?>
+<div class="page">
+  <div class="wrapper">
+    <h1 class="title"><?php the_title() ?></h1>
+    <div class="content"><?php the_content(); ?></div>
+  </div>
+</div>
+<?php
 get_footer();
